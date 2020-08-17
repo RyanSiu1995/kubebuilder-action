1ec6a5d0dd89d11836d56941d9f232b4f9fb34c7
Ryan SIU <ryan.siu@cloverhealth.com>
2020-08-17 20:00:50 +0800

    Fix the js syntax

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const tc = require('@actions/tool-cache');
 const os = require('os');
 const path = require('path');
 
-try {
-  const version = core.getInput('version');
-  const osPlat = os.platform();
-  const osArch = os.arch();
-
-  core.info(`Going to install kubebuilder ${version} for ${osPlat}-${osArch}`)
-
-  const downloadUrl = `https://go.kubebuilder.io/dl/${version}/${osPlat}/${osArch}`
-  const downloadPath = await tc.downloadTool(downloadPath, undefined, undefined);
+async function run() {
+  try {
+    const version = core.getInput('version');
+    const osPlat = os.platform();
+    const osArch = os.arch();
   
-  var extPath = ""
-  if (arch === 'win32') {
-    extPath = await tc.extractZip(downloadPath);
-  } else {
-    extPath = await tc.extractTar(downloadPath);
+    core.info(`Going to install kubebuilder ${version} for ${osPlat}-${osArch}`)
+  
+    const downloadUrl = `https://go.kubebuilder.io/dl/${version}/${osPlat}/${osArch}`
+    const downloadPath = await tc.downloadTool(downloadUrl, undefined, undefined);
+    
+    var extPath = ""
+    if (arch === 'win32') {
+      extPath = await tc.extractZip(downloadPath);
+    } else {
+      extPath = await tc.extractTar(downloadPath);
+    }
+  
+    const cachedDir = await tc.cacheDir(
+      extPath,
+      'kubebuilder',
+      version
+    );
+    core.info(`Successfully cached kubebuilder to ${cachedDir}`);
+  
+    const bp = path.join(cachedDir, 'bin');
+    core.addPath(bp);
+    
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  const cachedDir = await tc.cacheDir(
-    extPath,
-    'kubebuilder',
-    version
-  );
-  core.info(`Successfully cached kubebuilder to ${cachedDir}`);
-
-  const bp = path.join(cachedDir, 'bin');
-  core.addPath(bp);
-  
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run()
