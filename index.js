35895cdf6d7cc2a7fc8e59ff0ced9d10e984723d
Ryan SIU <ryan.siu@cloverhealth.com>
2020-08-17 20:17:08 +0800

    Try to fix the 404 error during the download

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 const os = require('os');
 const path = require('path');
+const fs = require('fs');
+const child_process = require('child_process');
+
+const supportedCombination = ["darwin-amd64", "linux-amd64", "linux-arm64", "linux-ppc64le"];
+const installedBinary = ["kubectl", "kube-apisever", "kubebuilder", "etcd"];
 
 async function run() {
   try {
     const version = core.getInput('version');
     const osPlat = os.platform();
-    const osArch = os.arch();
-  
-    core.info(`Going to install kubebuilder ${version} for ${osPlat}-${osArch}`)
-  
-    const downloadUrl = `https://go.kubebuilder.io/dl/${version}/${osPlat}/${osArch}`
-    const downloadPath = await tc.downloadTool(downloadUrl, undefined, undefined);
-    
-    var extPath = ""
-    if (arch === 'win32') {
-      extPath = await tc.extractZip(downloadPath);
-    } else {
-      extPath = await tc.extractTar(downloadPath);
+    var osArch = os.arch();
+    if (osArch === "x64") {
+      osArch = "amd64";
+    }
+    if (!supportedCombination.includes(`${osPlat}-${osArch}`)) {
+      throw `${osPlat}-${osArch} is not supported by this action now`;
     }
   
-    const cachedDir = await tc.cacheDir(
-      extPath,
-      'kubebuilder',
-      version
-    );
-    core.info(`Successfully cached kubebuilder to ${cachedDir}`);
+    core.info(`Going to install kubebuilder ${version} for ${osPlat}-${osArch}`);
   
-    const bp = path.join(cachedDir, 'bin');
-    core.addPath(bp);
+    await child_process.exec('sudo mkdir -p /usr/local/kubebuilder/bin/')
+
+    const downloadUrl = `https://go.kubebuilder.io/dl/${version}/${osPlat}/${osArch}`;
+    const downloadPath = await tc.downloadTool(downloadUrl, undefined, undefined);
     
+    const extPath = await tc.extractTar(downloadPath);
+  
+    await child_process.exec(`sudo cp ${extPath}/bin/ /usr/local/kubebuilder/bin/`)
   } catch (error) {
     core.setFailed(error.message);
   }
