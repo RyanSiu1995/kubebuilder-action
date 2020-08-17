f1ad60455e618bf02495bdda3d00188cbf781669
Ryan SIU <ryan.siu@cloverhealth.com>
2020-08-17 19:35:40 +0800

    Add test on the master

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,3 @@ curl -L https://go.kubebuilder.io/dl/${VERSION}/${OS}/${ARCH} | tar -xz -C /tmp/
 # move to a long-term location and put it on your path
 # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
 sudo mv /tmp/kubebuilder_${VERSION}_${OS}_${ARCH} /usr/local/kubebuilder
-export PATH=$PATH:/usr/local/kubebuilder/bin
