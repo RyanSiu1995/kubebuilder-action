a883b34d6e7ab119639c5109fff30996c56c6b6a
Ryan, Siu Long Wa <ryansiu1995@gmail.com>
2023-07-08 14:52:23 +0000

    Allow to remove sudo command

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run() {
     const kubebuilderOnly = core.getInput('kubebuilderOnly') === 'true';
     let etcdVersion = core.getInput('etcdVersion');
     let kubernetesVersion = core.getInput('kubernetesVersion');
+    let sudo = core.getInput('useSudo') ? 'sudo' : '';
     const majorVersion = version.split(".")[0];
 
     if (kubebuilderOnly && etcdVersion) {
@@ -53,9 +54,9 @@ async function run() {
     if (majorVersion > 2) {
       core.debug(`MajorVersion is greater than 2`);
       const downloadUrl = `https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${version}/kubebuilder_${osPlat}_${osArch}`;
-      execSync(`sudo mkdir -p /usr/local/kubebuilder/bin`);
-      execSync(`sudo curl -L ${downloadUrl} -o /usr/local/kubebuilder/bin/kubebuilder`);
-      execSync(`sudo chmod +x /usr/local/kubebuilder/bin/kubebuilder`);
+      execSync(`${sudo} mkdir -p /usr/local/kubebuilder/bin`);
+      execSync(`${sudo} curl -L ${downloadUrl} -o /usr/local/kubebuilder/bin/kubebuilder`);
+      execSync(`${sudo} chmod +x /usr/local/kubebuilder/bin/kubebuilder`);
       if (!kubebuilderOnly) {
         // Install kubectl and kube-apiserver
         ["kubectl", "kube-apiserver"].map((binary) => {
@@ -67,26 +68,26 @@ async function run() {
             execSync(`curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${osPlat}/${osArch}/${binary}"`);
           }
           execSync(`chmod +x ${binary}`);
-          execSync(`sudo mv ${binary} /usr/local/kubebuilder/bin`);
+          execSync(`${sudo} mv ${binary} /usr/local/kubebuilder/bin`);
         })
         // Install etcd
         core.info(`Going to install etcd ${etcdVersion}`);
         execSync(`curl -L https://github.com/etcd-io/etcd/releases/download/${etcdVersion}/etcd-${etcdVersion}-${osPlat}-${osArch}.tar.gz | tar -xz --strip-components=1 -C /tmp/`);
-        execSync(`sudo mv /tmp/etcd /usr/local/kubebuilder/bin`);
+        execSync(`${sudo} mv /tmp/etcd /usr/local/kubebuilder/bin`);
       } else {
         core.debug(`No extra binary will be installed.`);
       }
     } else {
       const downloadUrl = `https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${version}/kubebuilder_${version}_${osPlat}_${osArch}.tar.gz`;
       execSync(`curl -L ${downloadUrl} | tar -xz -C /tmp/`);
-      execSync(`sudo mv /tmp/kubebuilder_${version}_${osPlat}_${osArch}/ /usr/local/kubebuilder/`);
+      execSync(`${sudo} mv /tmp/kubebuilder_${version}_${osPlat}_${osArch}/ /usr/local/kubebuilder/`);
       execSync(`ls -la /usr/local/kubebuilder/bin`);
       if (kubebuilderOnly) {
         installedBinary
           .filter(x => x !== "kubebuilder")
           .map(x => {
             core.info(`Going to remove ${x}`);
-            execSync(`sudo rm /usr/local/kubebuilder/bin/${x}`);
+            execSync(`${sudo} rm /usr/local/kubebuilder/bin/${x}`);
           });
       } else {
         core.debug(`No extra binary will be deleted.`);
